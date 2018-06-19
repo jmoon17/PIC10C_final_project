5d4390d4fa053e1ecaa6c8855aac2c52ff8de190
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-19 15:03:31 -0700

    prevent the plane to move off the screen

--- plane.cpp
+++ plane.cpp
@@ -7,9 +7,11 @@
 void Plane::keyPressEvent(QKeyEvent *event)
 {
     if(event->key() == Qt::Key_Left){
+        if(pos().x() > 0)
         setPos(x()-20,y());
     }
     else if(event->key() == Qt::Key_Right){
+        if(pos().x() + 100 < 800)
         setPos(x()+20,y());
     }
 
