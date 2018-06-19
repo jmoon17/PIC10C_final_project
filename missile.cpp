57b4a821e8eab0a40c7d834c8ed8bd3ad0c0df5e
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-19 14:19:30 -0700

    move function in missile.h is implemented

--- missile.cpp
+++ missile.cpp
@@ -1,6 +1,20 @@
 #include "missile.h"
+#include <QTimer>
 
 Missile::Missile()
 {
+    //draw the missile
     setRect(0,0,15,50);
+
+    //connect
+    QTimer *timer = new QTimer();
+    connect(timer, SIGNAL(timeout()),this, SLOT(move()));   //every timeout, the move function is called
+
+    timer->start(50);       //every 50milleseconds, missile will move
+}
+
+void Missile::move()
+{
+    //move missile upwards
+    setPos(x(),y()-10);
 }
