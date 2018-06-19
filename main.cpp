c12371886348d8253ee6e1da0a29213549f98a51
Johan Moon <Moon@Johans-MacBook-Pro.local>
2018-06-19 11:37:12 -0700

    created QGraphicsScene and display the rect to the console

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-#include "mainwindow.h"
 #include <QApplication>
+#include <QGraphicsScene>
+#include <QGraphicsRectItem>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QGraphicsScene *scene = new QGraphicsScene();
+
+    QGraphicsRectItem *plane = new QGraphicsRectItem;
+    plane->setRect(0,0,100,100);    //set the rectangle with width and length of 100
+
+    scene->addItem(plane);
+
+    QGraphicsView *view = new QGraphicsView(scene);
+    view->show();   //display the rectangle(plane)
+
+
 
     return a.exec();
 }
